388afe287e35b4f2282e09b78fc2ddbc0f6f981c
S1ckret <S1ckret@users.noreply.github.com>
2019-09-14 19:49:46 +0300

    Imgui integrated

--- OpenGlSPE/src/Renderer.cpp
+++ OpenGlSPE/src/Renderer.cpp
@@ -15,3 +15,16 @@ bool GLLogCall(const char * function, const char * file, int line)
 	}
 	return true;
 }
+
+void Renderer::Clear() const
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void Renderer::Draw(const VertexArray & va, const IndexBuffer & ib, const Shader & shader) const
+{
+	va.Bind();
+	ib.Bind();
+	shader.Bind();
+	GLCall(glDrawElements(GL_LINE_LOOP, ib.getCount(), GL_UNSIGNED_INT, nullptr));
+}
--- OpenGlSPE/src/main.cpp
+++ OpenGlSPE/src/main.cpp
@@ -10,6 +10,10 @@
 #include "VertexBufferLayout.h"
 #include "Shader.h"
 
+#include "vendor\imgui\imgui.h"
+#include"vendor\imgui\imgui_impl_opengl3.h"
+#include "vendor\imgui\imgui_impl_glfw.h"
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -72,20 +76,52 @@ int main(void)
 		ib.Unbind();
 		shader.Unbind();
 
+		Renderer renderer;
+
+		ImGui::CreateContext();
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		ImGui::StyleColorsDark();
+		ImGui_ImplOpenGL3_Init("#version 130");
+
 		float r = 0.f;
 		float inc = 0.01f;
+		// Our state
+		bool show_demo_window = true;
+		bool show_another_window = false;
+		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window))
 		{
 			/* Render here */
-			glClear(GL_COLOR_BUFFER_BIT);
-
-			shader.Bind();
+			renderer.Clear();
 			shader.setUniform4f("u_Color", r, 0.0, 0.5, 1.0);
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+			{
+				static float f = 0.0f;
+				static int counter = 0;
 
-			va.Bind();
-			ib.Bind();
-			GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+				ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+				ImGui::Checkbox("Another Window", &show_another_window);
+
+				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+				if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+					counter++;
+				ImGui::SameLine();
+				ImGui::Text("counter = %d", counter);
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+				ImGui::End();
+			}
+
+			renderer.Draw(va, ib, shader);
 			if (r > 1.0) {
 				inc = -0.01f;
 			}
@@ -94,6 +130,8 @@ int main(void)
 			}
 
 			r += inc;
+			ImGui::Render();
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 			/* Swap front and back buffers */
 			glfwSwapBuffers(window);
@@ -103,6 +141,10 @@ int main(void)
 		}
 
 	}
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
 	glfwTerminate();
 	return 0;
 }
